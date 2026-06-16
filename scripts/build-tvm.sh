86f94cc8e26b183caa2a898a11857e17a3f02972
Rongcui Dong <rongcuid@outlook.com>
2026-06-16 16:08:37 +0000

    Change to heredoc for better compat

--- scripts/build-tvm.sh
+++ scripts/build-tvm.sh
@@ -11,18 +11,18 @@ uv pip install pytest numpy cython tornado psutil 'xgboost>=1.1.0' cloudpickle
 cmake -E rm -rf vendor/tvm/build
 cmake -E make_directory vendor/tvm/build
 cp vendor/tvm/cmake/config.cmake vendor/tvm/build/config.cmake
-{
-    printf 'set(CMAKE_BUILD_TYPE RelWithDebInfo)\n'
-    printf 'set(USE_LLVM "llvm-config --ignore-libllvm --link-static")\n'
-    printf 'set(HIDE_PRIVATE_SYMBOLS ON)\n'
-    printf 'set(USE_CUDA   OFF)\n'
-    printf 'set(USE_METAL   OFF)\n'
-    printf 'set(USE_VULKAN   OFF)\n'
-    printf 'set(USE_OPENCL   OFF)\n'
-    printf 'set(USE_CUBLAS   OFF)\n'
-    printf 'set(USE_CUDNN   OFF)\n'
-    printf 'set(USE_CUTLASS   OFF)\n'
-} >> vendor/tvm/build/config.cmake
+cat >> vendor/tvm/build/config.cmake <<EOF
+set(CMAKE_BUILD_TYPE RelWithDebInfo)
+set(USE_LLVM "llvm-config --ignore-libllvm --link-static")
+set(HIDE_PRIVATE_SYMBOLS ON)
+set(USE_CUDA   OFF)
+set(USE_METAL   OFF)
+set(USE_VULKAN   OFF)
+set(USE_OPENCL   OFF)
+set(USE_CUBLAS   OFF)
+set(USE_CUDNN   OFF)
+set(USE_CUTLASS   OFF)
+EOF
 cmake -S vendor/tvm -B vendor/tvm/build -GNinja
 cmake --build vendor/tvm/build --parallel "$(nproc)"
 pushd vendor/tvm/3rdparty/tvm-ffi; python3 -m pip install .; popd
