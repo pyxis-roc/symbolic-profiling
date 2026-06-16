0727870682e8ab88b5a797891c428163bf003ae9
Rongcui Dong <rongcuid@outlook.com>
2026-06-16 14:46:24 +0000

    Adding in dependencies

---
+++ scripts/build-llvm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cmake -E rm -rf vendor/tvm/build
+cmake -E make_directory vendor/tvm/build
+cp vendor/tvm/cmake/config.cmake vendor/tvm/build/config.cmake
+{
+    printf 'set(CMAKE_BUILD_TYPE RelWithDebInfo)\n'
+    printf 'set(USE_LLVM "llvm-config --ignore-libllvm --link-static")\n'
+    printf 'set(HIDE_PRIVATE_SYMBOLS ON)\n'
+    printf 'set(USE_CUDA   OFF)\n'
+    printf 'set(USE_METAL   OFF)\n'
+    printf 'set(USE_VULKAN   OFF)\n'
+    printf 'set(USE_OPENCL   OFF)\n'
+    printf 'set(USE_CUBLAS   OFF)\n'
+    printf 'set(USE_CUDNN   OFF)\n'
+    printf 'set(USE_CUTLASS   OFF)\n'
+} >> vendor/tvm/build/config.cmake
+cmake -S vendor/tvm -B vendor/tvm/build -GNinja
+cmake --build vendor/tvm/build --parallel "$(nproc)"
+pushd vendor/tvm/3rdparty/tvm-ffi; python -m pip install .; popd
