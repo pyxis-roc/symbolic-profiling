43d1c31ef22410bae271116378962c787446cbae
Rongcui Dong <rongcuid@outlook.com>
2026-06-17 02:43:52 +0000

    Update installation script

--- scripts/build-trip_counter.sh
+++ scripts/build-trip_counter.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+PREFIX="$PWD/.venv"
+
 if [[ -z "${VIRTUAL_ENV:-}" ]]; then
     [[ ! -e .venv ]] && uv venv --seed .venv
     source .venv/bin/activate
@@ -11,5 +13,6 @@ pushd trip_counter
 rm -rf build && mkdir -pv build
 cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -S . -B build
 cmake --build build
+cmake --install build --prefix "$PREFIX"
 
 popd
